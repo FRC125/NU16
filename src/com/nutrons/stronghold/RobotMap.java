b041454cce95984bc3286b42c8a8e587aae4c389
camilo <cgs.camilogonzalez@gmail.com>
2016-01-20 20:24:54 -0500

    added encoders

--- src/com/nutrons/stronghold/RobotMap.java
+++ src/com/nutrons/stronghold/RobotMap.java
@@ -10,6 +10,10 @@ public class RobotMap {
 	// PWM
 	public static int LEFT_DRIVE = 4;
 	public static int RIGHT_DRIVE = 3;
+	public static int LEFT_DRIVE_ENCODER_A = 5;
+	public static int LEFT_DRIVE_ENCODER_B = 6;
+	public static int RIGHT_DRIVE_ENCODER_A = 4;
+	public static int RIGHT_DRIVE_ENCODER_B = 3;
 	
 	// OI
 	public static int DRIVER_PAD = 0;
--- src/com/nutrons/stronghold/subsystems/Drivetrain.java
+++ src/com/nutrons/stronghold/subsystems/Drivetrain.java
@@ -3,6 +3,8 @@ package com.nutrons.stronghold.subsystems;
 
 import com.nutrons.stronghold.RobotMap;
 import com.nutrons.stronghold.commands.drivetrain.TankDriveCmd;
+
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -13,9 +15,22 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Drivetrain extends Subsystem {
     
-    private Talon leftDrive = new Talon(RobotMap.LEFT_DRIVE);
+    // Motors
+	private Talon leftDrive = new Talon(RobotMap.LEFT_DRIVE);
     private Talon rightDrive = new Talon(RobotMap.RIGHT_DRIVE);
 	
+    // Sensors
+    private Encoder leftDriveEncoder = new Encoder(RobotMap.LEFT_DRIVE_ENCODER_A, RobotMap.LEFT_DRIVE_ENCODER_B);
+    private Encoder rightDriveEncoder = new Encoder(RobotMap.RIGHT_DRIVE_ENCODER_A, RobotMap.RIGHT_DRIVE_ENCODER_B);
+    
+    // Constants
+    public static final double wheelDiam = 0.5;
+    
+    public Drivetrain() {
+    	this.leftDriveEncoder.setDistancePerPulse((wheelDiam * Math.PI)/256.0);
+    	this.rightDriveEncoder.setDistancePerPulse((wheelDiam * Math.PI)/256.0);
+    }
+    
     public void initDefaultCommand() {
         setDefaultCommand(new TankDriveCmd());
     }
@@ -35,6 +50,22 @@ public class Drivetrain extends Subsystem {
      */
     public void stop() {
     	this.driveLR(0, 0);
+    }
+    
+    /**
+     * Returns the distance traveled by the left side of the drivetrain in feet
+     * @return Distance in feet
+     */
+    public double getLeftDistanceInFeet() {
+    	return this.leftDriveEncoder.getDistance();
+    }
+    
+    /**
+     * Returns the distance traveled by the right side of the drivetrain in feet
+     * @return Distance in feet
+     */
+    public double getRightDistanceInFeet() {
+    	return this.rightDriveEncoder.getDistance();
     }
 }
 
