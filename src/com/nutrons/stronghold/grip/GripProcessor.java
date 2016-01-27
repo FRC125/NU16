f00857b5c92dd4671dcf228a52583cd97013be79
totallyashes <totally.ashes@gmail.com>
2016-01-26 19:43:14 -0500

    removed RoboMap references from GripProcessor

--- src/com/nutrons/stronghold/grip/GripProcessor.java
+++ src/com/nutrons/stronghold/grip/GripProcessor.java
@@ -4,22 +4,31 @@ import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import com.nutrons.stronghold.RobotMap;
-
 public class GripProcessor {
 	private static final int gripForceTerminateDelay = 10000;
 	
 	private Process process;
+	private String profileLocation;
+	private String javaLocation;
 
-	public boolean startGrip(String filename){
+	private String gripLocation;
+	
+	public GripProcessor(String javaLocation, String gripLocation, String profileLocation){
+		this.javaLocation = javaLocation;
+		this.profileLocation = profileLocation;
+		this.gripLocation = gripLocation;
+	}
+	/** starts grip **/
+	public boolean startGrip(){
 		try {
-			this.process = Runtime.getRuntime().exec(new String[]{RobotMap.JAVA_LOCATION, "-jar", "/home/lvuser/grip.jar", filename});
+			this.process = Runtime.getRuntime().exec(new String[]{this.javaLocation, "-jar", this.gripLocation, this.profileLocation});
 		} catch (IOException e) {
 			e.printStackTrace();
 			return false;
 		}
 		return true;
 	} 
+	/** stops grip, after a delay will forcibly terminate grip **/
 	public void stopGrip(){ 
 		this.process.destroy();
 		new Timer().schedule(new TimerTask() {
