9fe8a8a1e54afd85b5aeb1522b519f2a6d8cbde9
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2016-02-03 08:32:41 -0500

    Rewrote deadband

--- src/com/nutrons/lib/utils/Utils.java
+++ src/com/nutrons/lib/utils/Utils.java
@@ -2,23 +2,13 @@ package com.nutrons.lib.utils;
 
 /**
  * 
- * @author Lydia Xing
+ * @author Lydia Xing, Camilo Gonzalez
  *
  */
 
 public class Utils {
 
-	public static double deadBand(double input, double deadband) {
-		if (input > 1.0) {
-			input = 1.0;
-		} else if (input < -1.0) {
-			input = -1.0;
-		} else if (Math.abs(input) <= deadband) {
-			input = 0.0;
-		} else {
-			input = 0.0;
-		}
-
-		return input;
-	}
+	public static double deadband(double value, double deadband, double center) {
+        return (value < (center + deadband) && value > (center - deadband)) ? center : value;
+    }
 }
