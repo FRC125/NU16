78b092a92c8f8f2d7ee274b935d3a5d882262dbc
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2016-02-06 12:25:51 -0500

    added spit ball sequence

---
+++ src/com/nutrons/stronghold/commands/intake/SpitBall.java
@@ -0,0 +1,22 @@
+package com.nutrons.stronghold.commands.intake;
+
+import com.nutrons.stronghold.commands.shooter.MoveArmToIntakePositionCmd;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
+
+/**
+ * 
+ * @author Abdul Mezrouh, Camilo Gonzalez, Andreas Jeonard
+ *
+ */
+public class SpitBall extends CommandGroup {
+    
+    public  SpitBall() {
+    	addSequential(new MoveArmToIntakePositionCmd());
+    	addSequential(new RollersOutCmd());
+    	addSequential(new WaitCommand(0.5));
+    	addSequential(new OpenJawCmd());
+    	addSequential(new StopIntakeMotorCmd());
+    }
+}
