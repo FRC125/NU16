8ef5334a5eb64fd3987ecab2d56b3886419d9246
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2016-02-07 18:45:53 -0500

    added arm motor

--- src/com/nutrons/stronghold/RobotMap.java
+++ src/com/nutrons/stronghold/RobotMap.java
@@ -7,18 +7,6 @@ package com.nutrons.stronghold;
  */
 public class RobotMap {
     
-	// PWM
-	public static int LEFT_DRIVE = 4;
-	public static int RIGHT_DRIVE = 3;
-	public static int LEFT_DRIVE_ENCODER_A = 5;
-	public static int LEFT_DRIVE_ENCODER_B = 6;
-	public static int RIGHT_DRIVE_ENCODER_A = 4;
-	public static int RIGHT_DRIVE_ENCODER_B = 3;
-	
-	// OI
-	public static int DRIVER_PAD = 0;
-	
-	// Digital
-	public static int LEFT_SOLENOID = 7;
-	public static int RIGHT_SOLENOID = 0;
+	// CAN
+	public static final int ARM_MOTOR = 1;
 }
--- src/com/nutrons/stronghold/subsystems/Shooter.java
+++ src/com/nutrons/stronghold/subsystems/Shooter.java
@@ -1,5 +1,9 @@
 package com.nutrons.stronghold.subsystems;
 
+import com.nutrons.stronghold.RobotMap;
+
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.CANTalon.TalonControlMode;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -8,13 +12,19 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  *
  */
 public class Shooter extends Subsystem {
+	
+	private CANTalon arm = new CANTalon(RobotMap.ARM_MOTOR);
 
 	public Shooter() {
-		
+		this.arm.changeControlMode(TalonControlMode.PercentVbus);
 	}
 	
 	public void initDefaultCommand() {
     
+	}
+	
+	public void driveArm(double power) {
+		this.arm.set(power);
 	}
 }
 
