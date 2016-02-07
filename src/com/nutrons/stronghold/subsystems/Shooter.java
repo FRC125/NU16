beb25816dfd0213de3a6bef7e09bb259b8559e79
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2016-02-07 18:43:42 -0500

    added Shooter subsystem

---
+++ src/com/nutrons/stronghold/subsystems/Shooter.java
@@ -0,0 +1,20 @@
+package com.nutrons.stronghold.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * 
+ * @author Camilo Gonzalez
+ *
+ */
+public class Shooter extends Subsystem {
+
+	public Shooter() {
+		
+	}
+	
+	public void initDefaultCommand() {
+    
+	}
+}
+
