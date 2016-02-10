cedb5d2d8796e39cdafa38c8dc86ed252ddbfdbc
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2016-02-10 11:53:59 -0500

    created motors and initialized them to percentVBus by default

--- src/com/nutrons/stronghold/subsystems/Drivetrain.java
+++ src/com/nutrons/stronghold/subsystems/Drivetrain.java
@@ -1,6 +1,10 @@
 package com.nutrons.stronghold.subsystems;
 
 import com.kauailabs.navx_mxp.AHRS;
+import com.nutrons.stronghold.RobotMap;
+
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.CANTalon.TalonControlMode;
 import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.SerialPort.Port;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -12,6 +16,12 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Drivetrain extends Subsystem {
     
+	// Motors
+	private CANTalon leftDriveA = new CANTalon(RobotMap.LEFT_DRIVE_MOTOR_A);
+	private CANTalon leftDriveB = new CANTalon(RobotMap.LEFT_DRIVE_MOTOR_B);
+	private CANTalon rightDriveA = new CANTalon(RobotMap.RIGHT_DRIVE_MOTOR_A);
+	private CANTalon rightDriveB = new CANTalon(RobotMap.RIGHT_DRIVE_MOTOR_B);
+	
 	// navx gyro
 	private Byte update_rate_hz = 50;
     private SerialPort serialPort;
@@ -19,10 +29,12 @@ public class Drivetrain extends Subsystem {
     
     public Drivetrain() {
     	
+    	this.setPercentDrive();
+    	
     	try {
     		Port p = SerialPort.Port.kMXP;
-    		serialPort = new SerialPort(57600, p);
-    		imu = new AHRS(serialPort, update_rate_hz);
+    		this.serialPort = new SerialPort(57600, p);
+    		this.imu = new AHRS(this.serialPort, this.update_rate_hz);
     	}catch(Exception e) {
     		System.err.println("Cannot initilize Navx in Drivetrain Subsystem!!!");
     	}
@@ -45,5 +57,27 @@ public class Drivetrain extends Subsystem {
      */
     public void resetGyro() {
     	this.imu.zeroYaw();
+    }
+    
+    /**
+     * Sets up drivetrain mode to percentVBus (aka regular -1 to 1)
+     */
+    public void setPercentDrive() {
+    	this.leftDriveA.changeControlMode(TalonControlMode.PercentVbus);
+    	this.leftDriveB.changeControlMode(TalonControlMode.PercentVbus);
+    	this.rightDriveA.changeControlMode(TalonControlMode.PercentVbus);
+    	this.rightDriveB.changeControlMode(TalonControlMode.PercentVbus);
+    	
+    	this.leftDriveA.configNominalOutputVoltage(+0.0f, -0.0f); 
+		this.leftDriveA.configPeakOutputVoltage(+12.0f, 0.0f);
+		
+		this.leftDriveB.configNominalOutputVoltage(+0.0f, -0.0f); 
+		this.leftDriveB.configPeakOutputVoltage(+12.0f, 0.0f);
+		
+		this.rightDriveA.configNominalOutputVoltage(+0.0f, -0.0f); 
+		this.rightDriveA.configPeakOutputVoltage(+12.0f, 0.0f);
+		
+		this.rightDriveB.configNominalOutputVoltage(+0.0f, -0.0f); 
+		this.rightDriveB.configPeakOutputVoltage(+12.0f, 0.0f);
     }
 }
